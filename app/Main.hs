2ca1e48e501120e8ebdf45af598bb518e892748d
Isaac Elliott <isaace71295@gmail.com>
2017-06-09 16:06:31 +1000

    Fixed code, will add comments in pull request

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,26 @@
-{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import           Control.Monad.State.Lazy
 import           Data.Char
+import           Data.Maybe
 import           System.Environment
-
-initialCellState = replicate 30000 (0 :: Int)
 
 type CellState = [Int]
 
--- MoveState :: Int (Position) -> Parsed Commands -> CellState
-type MoveState = (Int, [Command], CellState)
+data MoveState
+  = MoveState
+  { position :: Int
+  , commands :: [Command]
+  , cells :: CellState
+  } deriving Show
+
+initialMoveState :: MoveState
+initialMoveState
+  = MoveState
+  { position = 0
+  , commands = []
+  , cells = replicate 30000 0
+  }
 
 data Command = MoveRight
              | MoveLeft
@@ -23,61 +33,82 @@ data Command = MoveRight
              deriving (Show, Eq)
 
 tokenize :: String -> [Command]
-tokenize [] = []
-tokenize (x:xs)
-  | x == '>' = MoveRight : tokenize xs
-  | x == '<' = MoveLeft : tokenize xs
-  | x == '+' = IncCell: tokenize xs
-  | x == '-' = DecCell : tokenize xs
-  | x == '.' = PrintCell : tokenize xs
-  | x == ',' = InputCell: tokenize xs
-  | x == '[' = OpenBracket : tokenize xs
-  | x == ']' = CloseBracket: tokenize xs
-  | otherwise = tokenize xs
+tokenize = catMaybes . fmap charToCommand
+  where
+    charToCommand c =
+      case c of
+        '>' -> Just MoveRight
+        '<' -> Just MoveLeft
+        '+' -> Just IncCell
+        '-' -> Just DecCell
+        '.' -> Just PrintCell
+        ',' -> Just InputCell
+        '[' -> Just OpenBracket
+        ']' -> Just CloseBracket
+        _ -> Nothing
 
-
--- Sets a specific cell to a value
 setCell :: (Int -> Int) -> CellState -> Int -> CellState
-setCell f cs i = take i cs ++ [if j < 0 then 255 else j] ++ drop (i + 1) cs
-    where j = f $ cs !! i
+setCell _ [] _ = []
+setCell f (c:rest) 0 = f c : rest
+setCell f (c:rest) n = c : setCell f rest (n-1)
 
 incCell :: CellState -> Int -> CellState
 incCell = setCell (+1)
 
 decCell :: CellState -> Int -> CellState
-decCell = setCell (\x -> x - 1)
-
+decCell = setCell (subtract 1)
 
 -- Parse
 -- Close bracket is different from the others as it
 -- doesn't bind itself to the parse process
 parse :: [Command] -> StateT MoveState IO [Command]
-parse [] = StateT (\x -> pure ([], x))
-parse (x:xs) = if x == CloseBracket then s else s >>= parse
-    where s = StateT (\v@(i, pc, cs) ->
-                if i < 0 then error $ show v
-                else
-                case x of
-                    MoveRight    -> pure (xs, (i + 1, pc ++ [x], cs))
-                    MoveLeft     -> pure (xs, (i - 1, pc ++ [x], cs))
-                    IncCell      -> pure (xs, (i, pc ++ [x], incCell cs i))
-                    DecCell      -> pure (xs, (i, pc ++ [x], decCell cs i))
-                    OpenBracket  -> do (xs', (i', pc', cs')) <- liftIO $ runStateT (parse xs) (i, [], cs)
-                                       pure (xs', (i, pc ++ [x] ++ pc', cs'))
-                    CloseBracket -> if cs !! i > 0
-                                    then
-                                       do (xs', ms') <- liftIO $ runStateT (parse (pc ++ [x] ++ xs)) (i, [], cs)
-                                          pure (xs', ms')
-                                    else pure (xs, (i, pc ++ [x], cs))
-                    InputCell    -> do c <- liftIO getChar
-                                       pure (xs, (i, pc ++ [x], setCell (const $ fromEnum c) cs i))
-                    PrintCell    -> do liftIO $ print (chr (cs !! i))
-                                       return (xs, (i, pc ++ [x], cs))
-            )
+parse [] = pure [] 
+parse (x:xs) =
+  case x of
+    CloseBracket -> s
+    _ -> s >>= parse
+  where
+    s = do
+      ms@(MoveState pos cmds cls) <- get 
+      when (pos < 0) . error $ show ms
+      case x of
+        MoveRight -> do
+          put $ MoveState (pos + 1) (cmds ++ [x]) cls
+          pure xs
+        MoveLeft -> do
+          put $ MoveState (pos - 1) (cmds ++ [x]) cls
+          pure xs
+        IncCell -> do
+          put $ MoveState pos (cmds ++ [x]) (incCell cls pos)
+          pure xs
+        DecCell -> do
+          put $ MoveState pos (cmds ++ [x]) (decCell cls pos)
+          pure xs 
+        OpenBracket -> do
+          modify $ \ms -> ms { commands = [] }
+          xs' <- parse xs
+          MoveState _ cmds' cls' <- get
+          put $ MoveState pos (cmds ++ [x] ++ cmds') cls'
+          pure xs'
+        CloseBracket ->
+          if cls !! pos > 0
+            then do
+              modify $ \ms -> ms { commands = [] }
+              parse (cmds ++ x:xs)
+            else do
+              put $ MoveState pos (cmds ++ [x]) cls
+              pure xs
+        InputCell -> do
+          c <- liftIO getChar
+          put $ MoveState pos (cmds ++ [x]) (setCell (const $ fromEnum c) cls pos)
+          pure xs
+        PrintCell -> do
+          liftIO $ print (chr (cls !! pos))
+          put $ MoveState pos (cmds ++ [x]) cls
+          pure xs
 
 main :: IO ()
 main = do
     (file : _) <- getArgs
     s <- readFile file
-    (_, (_, _, cs)) <- runStateT (parse $ tokenize s) (0, [], initialCellState)
-    return ()
+    void $ runStateT (parse $ tokenize s) initialMoveState
